67659c3ce05d52188ba064ead304795bc6f31ae5
JeuxCol <ag693765@gmail.com>
2022-10-29 14:31:38 -0500

    Agregando el proyecto tienda virtual

---
+++ src/test/java/com/TiendaVirtual/app/TiendaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaVirtual.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
